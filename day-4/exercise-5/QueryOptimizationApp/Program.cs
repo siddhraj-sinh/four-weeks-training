ac71295fb11a4612d278f3636b11b7b901a44e56
Chintan Shah <chintanshah@promactinfo.com>
2023-05-07 17:05:50 +0530

    adds exercise 5 for day 4

---
+++ day-4/exercise-5/QueryOptimizationApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+
+namespace QueryOptimizationApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> data = GenerateRandomNumbers(10000000);
+            Stopwatch sw = Stopwatch.StartNew();
+            // Current implementation
+            var originalQuery = data.Where(x => x > 100).OrderByDescending(x => x).Take(10);
+            sw.Stop();
+            Console.WriteLine("Original Query: {0} ms", sw.ElapsedMilliseconds);
+
+            // Optimized implementation
+            sw.Restart();
+            
+            sw.Stop();
+            Console.WriteLine("Optimized Query: {0} ms", sw.ElapsedMilliseconds);
+        }
+
+        static List<int> GenerateRandomNumbers(int count)
+        {
+            Random random = new Random();
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < count; i++)
+            {
+                int randomNumber = random.Next();
+                numbers.Add(randomNumber);
+            }
+
+            return numbers;
+        }
+    }
+}
