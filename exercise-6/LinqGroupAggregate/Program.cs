7c769c513e0e6c0405248dcebc4aaa1fa90f4e36
Chintan Shah <chintanshah@promactinfo.com>
2023-05-07 12:04:12 +0530

    adds exercise 6

---
+++ exercise-6/LinqGroupAggregate/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace LinqGroupAggregate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a list of Product objects
+            List<Product> products = new List<Product>
+        {
+            new Product { Name = "Laptop", Category = "Electronics", Price = 1200.00M },
+            new Product { Name = "Smartphone", Category = "Electronics", Price = 800.00M },
+            new Product { Name = "Headphones", Category = "Electronics", Price = 200.00M },
+            new Product { Name = "Shirt", Category = "Clothing", Price = 30.00M },
+            new Product { Name = "Jeans", Category = "Clothing", Price = 60.00M },
+            new Product { Name = "Sneakers", Category = "Footwear", Price = 100.00M }
+        };
+
+            // Use LINQ to perform the following operations:
+            // 1. Group products by category
+            // 2. Count the number of products in each category
+            // 3. Calculate the total price of products in each category
+            // 4. Find the most expensive product in each category
+        }
+    }
+}
