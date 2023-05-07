25715dad277eeee4eddd580fd84e1257057538f2
Chintan Shah <chintanshah@promactinfo.com>
2023-05-07 16:39:05 +0530

    adds exercise 3 for day 4

---
+++ day-4/exercise-3/LinqToObjectsApp/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace LinqToObjectsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>
+            {
+                new Person { Name = "John", Age = 25, Country = "USA" },
+                new Person { Name = "Jane", Age = 30, Country = "Canada" },
+                new Person { Name = "Mark", Age = 28, Country = "USA" },
+                new Person { Name = "Emily", Age = 22, Country = "Australia" }
+            };
+
+
+            //Write queries using LINQ for following operations
+            //1. Get all people from USA
+            //2. Get all people above 30
+            //3. Sort people by name
+            //4. Project/Select only Name and Country of all people
+
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Country { get; set; }
+    }
+}
