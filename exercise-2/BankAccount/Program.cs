738959384cf193f07748a2afb240a857db2b4f08
Chintan Shah <chintanshah@promactinfo.com>
2023-05-07 11:59:24 +0530

    adds bank account and animal exercise

---
+++ exercise-2/BankAccount/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BankAccount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create SavingsAccount and CheckingAccount objects and perform operations
+        }
+    }
+}
---
+++ exercise-3/AnimalExercise/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AnimalExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a list of Animal objects, add Dog and Cat instances, and call their methods
+        }
+    }
+}
