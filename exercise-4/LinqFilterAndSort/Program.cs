e97cf23bc7a73b3b6ce89a0d1fd4b162e8bda198
Chintan Shah <chintanshah@promactinfo.com>
2023-05-07 12:00:48 +0530

    adds exercise 4

---
+++ exercise-4/LinqFilterAndSort/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LinqFilterAndSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
