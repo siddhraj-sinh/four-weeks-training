fd3766bf59db7a8df29b9bb80936432484c2f2ec
Chintan Shah <chintanshah@promactinfo.com>
2023-05-07 17:36:02 +0530

    adds exercise 1 for day 5

---
+++ week-1/day-5/exercise-1/FileLoggerDisposableApp/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace FileLoggerDisposableApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Use FileLogger and dispose of it properly
+        }
+    }
+
+    class FileLogger : IDisposable
+    {
+        private StreamWriter _writer;
+
+        public FileLogger(string filePath)
+        {
+            // Initialize StreamWriter instance
+        }
+
+        public void Dispose()
+        {
+            // Implement IDisposable pattern
+        }
+
+        public void Log(string message)
+        {
+            // Write message to the file
+        }
+    }
+}
