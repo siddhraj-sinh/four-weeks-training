6c2f74dc2e8dba015f3fbd871e4bbef5bde8cfaf
Chintan Shah <chintanshah@promactinfo.com>
2023-05-07 16:56:12 +0530

    adds exercise 4 for day 4

---
+++ day-4/exercise-4/LinqToXmlApp/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace LinqToXmlApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Assume there is an XML document with the following structure:
+            // <Books>
+            //     <Book>
+            //         <Title>Book Title 1</Title>
+            //         <Author>Author 1</Author>
+            //         <Genre>Genre 1</Genre>
+            //     </Book>
+            //     ...
+            // </Books>
+            // Write above book structure as a c# string
+            string xmlString = @"<Books>
+                                    <Book>
+                                        <Title>Book Title 1</Title>
+                                        <Author>Author 1</Author>
+                                        <Genre>Genre 1</Genre>
+                                    </Book>
+                                    <Book>
+                                        <Title>Book Title 2</Title>
+                                        <Author>Author 2</Author>
+                                        <Genre>Genre 2</Genre>
+                                    </Book>
+                                    <Book>
+                                        <Title>Book Title 3</Title>
+                                        <Author>Author 3</Author>
+                                        <Genre>Genre 3</Genre>
+                                    </Book>
+                                </Books>";
+
+            // Create an XDocument object from the XML string
+
+            // Write the title of all books to the console
+
+            // Write the title of all books with genre "Genre 1" to the console
+
+        }
+    }
+}
