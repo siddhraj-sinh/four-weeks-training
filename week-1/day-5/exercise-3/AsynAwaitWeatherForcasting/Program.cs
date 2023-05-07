df5d3e35209701c124f2b3e82ce9afa7514d5236
Chintan Shah <chintanshah@promactinfo.com>
2023-05-07 17:46:46 +0530

    adds exercise 4 for day 5 of week 1

---
+++ week-1/day-5/exercise-3/AsynAwaitWeatherForcasting/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace AsynAwaitWeatherForcasting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        // Call OpenWeatherMap API to fetch weather data https://openweathermap.org/api
+        // Create a C# object from the JSON response
+        // Replace Task<string> with the C# object Task<WeatherData>
+        static async Task<string> FetchWeatherDataAsync(string url)
+        {
+            // Fetch web page content asynchronously using HttpClient
+            throw new NotImplementedException();
+        }
+    }
+}
