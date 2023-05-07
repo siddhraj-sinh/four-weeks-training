149ed01f886855a99adc4eda7679f8fb6bdc978b
Chintan Shah <chintanshah@promactinfo.com>
2023-05-07 17:55:13 +0530

    adds exercise 3 for day 5 of week 1

---
+++ week-1/day-5/exercise-3/AsyncAwaitBasics/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace AsyncAwaitBasics
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Call PerformCalculations and measure time taken using Stopwatch
+        }
+
+        static async Task SimulateLongRunningTask(int delayInSeconds)
+        {
+            // Implement long-running task simulation
+        }
+
+        static async Task PerformCalculations(int numberOfTasks)
+        {
+            // Start long-running tasks concurrently and wait for them to complete
+        }
+    }
+}
