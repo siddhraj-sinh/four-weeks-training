40720b17e98c8e043e186413430967aab9359bd4
Chintan Shah <chintanshah@promactinfo.com>
2023-05-07 17:38:58 +0530

    adds exercise 2 for day 5 for week 1

---
+++ week-1/day-5/exercise-2/CovariantAndContravariantGenerics/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace CovariantAndContravariantGenerics
+{
+    interface IProcessor<in TInput, out TResult>
+    {
+        TResult Process(TInput input);
+    }
+
+    class StringToIntProcessor : IProcessor<string, int>
+    {
+        // Implement Process method
+        public int Process(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class DoubleToStringProcessor : IProcessor<double, string>
+    {
+        // Implement Process method
+        public string Process(double input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Demonstrate covariance and contravariance with IProcessor interface
+        }
+    }
+}
