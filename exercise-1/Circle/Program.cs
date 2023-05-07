f99cf5348430ee3b1aaa458eaf8a4c70129586fc
Chintan Shah <chintanshah@promactinfo.com>
2023-05-07 11:55:36 +0530

    adds circle projects

---
+++ exercise-1/Circle/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Circle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a Circle object and display its area and circumference
+        }
+    }
+}
