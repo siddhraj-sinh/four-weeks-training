7c690bf7607909909de4d1af8bd7b87e8c5abdea
Chintan Shah <chintanshah@promactinfo.com>
2023-05-07 16:35:21 +0530

    adds exercise 2 for day 4

---
+++ day-4/exercise-2/IteratorsApp/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace IteratorsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var fibonacci = FibonacciSequence().Take(10);
+            foreach (int number in fibonacci)
+            {
+                Console.WriteLine(number);
+            }
+        }
+        // https://www.c-sharpcorner.com/UploadFile/5ef30d/understanding-yield-return-in-C-Sharp/
+        // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/yield
+        public static IEnumerable<int> FibonacciSequence()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
