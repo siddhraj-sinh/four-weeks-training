15eec6c669e2aef6590db1da81cf90f542f72b2e
Chintan Shah <chintanshah@promactinfo.com>
2023-05-13 09:58:47 +0530

    adds a project for week 1

---
+++ week-1/project/LibraryManagement/LibraryManagement/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace LibraryManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialize collections for books, authors, and borrowers
+            List<Book> books = new List<Book>();
+            List<Author> authors = new List<Author>();
+            List<Borrower> borrowers = new List<Borrower>();
+
+            // Main program loop
+            while (true)
+            {
+                DisplayMenu();
+
+                int choice;
+                int.TryParse(Console.ReadLine(), out choice);
+
+                switch (choice)
+                {
+                    // Add cases for each menu option
+                    case 1:
+                        // Add a book
+                        break;
+                    // ...
+                    case 16:
+                        // Filter books by status
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option. Please try again.");
+                        break;
+                }
+            }
+        }
+
+        static void DisplayMenu()
+        {
+            // Display the menu options
+            Console.WriteLine("Welcome to the Library Management System!\n");
+            Console.WriteLine("1. Add a book");
+            Console.WriteLine("2. Update a book");
+            Console.WriteLine("3. Delete a book");
+            Console.WriteLine("4. List all books");
+            // ...Add Other options here
+            Console.WriteLine("16. Filter books by status");
+            Console.WriteLine("\nEnter the number corresponding to the action you'd like to perform:");
+        }
+    }
+
+    // Class definitions
+    class Book
+    {
+        public string Title { get; set; }
+        public Author Author { get; set; }
+        public int PublicationYear { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+
+    class Author
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+
+    class Borrower
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
