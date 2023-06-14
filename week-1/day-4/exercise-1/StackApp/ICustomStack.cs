e6e372c20a68bb4d09d3ec12879b1ae4fe508242
siddhraj-sinh <skrathod282001@gmail.com>
2023-06-14 14:31:33 +0530

    added generic stack class

--- week-1/day-4/exercise-1/StackApp/ICustomStack.cs
+++ week-1/day-4/exercise-1/StackApp/ICustomStack.cs
@@ -6,4 +6,39 @@
         T Pop();
         bool IsEmpty();
     }
+
+    public class Stack<T> : ICustomStack<T>
+    {
+
+
+        private class Node
+        {
+            public T Data { get; set; }
+            public Node Next { get; set; }
+            public Node(T data)
+            {
+                Data = data;
+                Next = null;
+            }
+        }
+        private Node top;
+
+        public void Push(T item)
+        {
+            Node newNode = new Node(item);
+            newNode.Next = top;
+            top = newNode;
+
+        }
+
+        public T Pop()
+        {
+            T data = top.Data;
+            top = top.Next;
+            return data;
+        }
+        public bool IsEmpty() {
+            return top == null;
+        }
+    }
 }
--- week-1/day-4/exercise-1/StackApp/Program.cs
+++ week-1/day-4/exercise-1/StackApp/Program.cs
@@ -2,15 +2,48 @@
 {
     internal class Program
     {
+
+
         static void Main(string[] args)
         {
-            //ICustomStack<int> intStack = new CustomStack<int>();
-            //intStack.Push(1);
-            //intStack.Push(2);
-            //intStack.Push(3);
-            //Console.WriteLine(intStack.Pop()); // Output: 3
-            //Console.WriteLine(intStack.Pop()); // Output: 2
-            //Console.WriteLine(intStack.IsEmpty()); // Output: False
+            Stack<int> intStack = new Stack<int>();
+            intStack.Push(1);
+            intStack.Push(2);
+            intStack.Push(3);
+            Console.WriteLine(intStack.Pop()); // Output: 3
+            Console.WriteLine(intStack.Pop()); // Output: 2
+            Console.WriteLine(intStack.IsEmpty()); // Output: False
+
+            Stack<String> stack = new Stack<String>();
+            stack.Push("A");
+            stack.Push("B");
+            stack.Push("C");
+            Console.WriteLine(stack.Pop()); // output: C
+
+            Stack<Person> personStack = new Stack<Person>();
+            personStack.Push(new Person("John", 25));
+            personStack.Push(new Person("Jane", 30));
+            Console.WriteLine("Popped item: " + personStack.Pop());
+            Console.ReadLine();
         }
+
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public override string ToString()
+        {
+            return $"{Name} : {Age}";
+        }
+
+
     }
 }
