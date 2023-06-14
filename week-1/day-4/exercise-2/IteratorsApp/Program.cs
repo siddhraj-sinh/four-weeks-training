bf6a27487ddecbaed6df107a89ca1e7a77a3e52c
siddhraj-sinh <skrathod282001@gmail.com>
2023-06-14 15:28:52 +0530

    completed FibonacciSequence iterator method

--- week-1/day-4/exercise-2/IteratorsApp/Program.cs
+++ week-1/day-4/exercise-2/IteratorsApp/Program.cs
@@ -9,12 +9,28 @@
             {
                 Console.WriteLine(number);
             }
+
+            //Console.WriteLine("Hello World");
+            Console.ReadLine();
         }
         // https://www.c-sharpcorner.com/UploadFile/5ef30d/understanding-yield-return-in-C-Sharp/
         // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/yield
         public static IEnumerable<int> FibonacciSequence()
         {
-            throw new NotImplementedException();
+            int first = 0;
+            int second = 1;
+
+            yield return first;
+            yield return second;
+
+            while (true)
+            {
+                int next = first + second;
+                yield return next;
+
+                first = second;
+                second = next;
+            }
         }
     }
 }
