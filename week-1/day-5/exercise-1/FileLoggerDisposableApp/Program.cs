e1364596d9a3fd3f2bd15b6e6ab80d5593c62108
siddhraj-sinh <skrathod282001@gmail.com>
2023-06-15 17:52:36 +0530

    completed exercise 1 day 5

--- week-1/day-5/exercise-1/FileLoggerDisposableApp/Program.cs
+++ week-1/day-5/exercise-1/FileLoggerDisposableApp/Program.cs
@@ -4,9 +4,17 @@
     {
         static void Main(string[] args)
         {
+            string fileName = @"C:\siddharaj\local\week-1\day-5\exercise-1\FileLoggerDisposableApp\log.txt";
             // Use FileLogger and dispose of it properly
+            using (var logger = new FileLogger(fileName))
+            {
+                logger.Log("Log entry 1");
+                logger.Log("Log entry 2");
+                logger.Log("Log entry 3");
+            }
         }
     }
+
 
     class FileLogger : IDisposable
     {
@@ -15,16 +23,36 @@
         public FileLogger(string filePath)
         {
             // Initialize StreamWriter instance
+            _writer = new StreamWriter(filePath);
         }
 
         public void Dispose()
         {
             // Implement IDisposable pattern
-        }
 
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (_writer!=null) {
+                    
+                    _writer.Dispose();
+                    _writer = null;
+                }
+            }
+        }
         public void Log(string message)
         {
             // Write message to the file
+            if (_writer == null)
+            {
+                throw new ObjectDisposedException("FileLogger", "The logger has been disposed.");
+            }
+            Console.WriteLine(message);
+            _writer.WriteLine(message);
         }
     }
 }
