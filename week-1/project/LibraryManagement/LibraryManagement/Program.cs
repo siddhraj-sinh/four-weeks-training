38aefbf667555e833f36cd08f6b3ca3555e4eff8
siddhraj-sinh <skrathod282001@gmail.com>
2023-06-17 12:20:04 +0530

    added more cases library managments

--- week-1/project/LibraryManagement/LibraryManagement/Program.cs
+++ week-1/project/LibraryManagement/LibraryManagement/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Xml.Linq;
+﻿using System.Runtime.CompilerServices;
+using System.Xml.Linq;
 
 namespace LibraryManagement
 {
@@ -27,14 +28,57 @@ namespace LibraryManagement
                         AddBook();
                         break;
                     case 2:
+                        //update a book
+                        UpdateBook();
                         break;
                     case 3:
+                        //Delete a book
                         DeleteBook();
                         break;
                     case 4:
+                        //list all book
                         ListAllBooks();
                         break;
-                    // ...
+                    case 5:
+                        //add author
+                        AddAuthor();
+                        break;
+                    case 6:
+                        //update author
+                        UpdateAuthor();
+                        break;
+                    case 7:
+                        //delete author
+                        DeleteAuthor();
+                        break;
+                    case 8:
+                        //list author
+                        ListAllAuthor();
+                        break;
+                    case 9:
+                        //add borrower
+                        AddBorrower();
+                        break;
+                    case 10:
+                        //Update borrowe
+                        UpdateBorrower();
+                        break;
+                    case 11:
+                        //delete borrower
+                        DeleteBorrower();
+                        break;
+                   case 12:
+                        //list borrower
+                        ListAllBorrower();
+                        break;
+                    case 13:
+                        //register book to a borrower
+                        BorrowBook();
+                        break;
+                    case 14:
+                        //list all borrowed books
+                        ListAllBorrowedBooks();
+                        break;
                     case 16:
                         // Filter books by status
                         FilterBookByStatus();
@@ -49,12 +93,21 @@ namespace LibraryManagement
         static void DisplayMenu()
         {
             // Display the menu options
-            Console.WriteLine("Welcome to the Library Management System!\n");
+            Console.WriteLine("********Welcome to the Library Management System!*******\n");
             Console.WriteLine("1. Add a book");
             Console.WriteLine("2. Update a book");
             Console.WriteLine("3. Delete a book");
             Console.WriteLine("4. List all books");
-            // ...Add Other options here
+            Console.WriteLine("5. Add a author");
+            Console.WriteLine("6. Update a author");
+            Console.WriteLine("7. Delete a author");
+            Console.WriteLine("8. List all authors");
+            Console.WriteLine("9. Add a borrower");
+            Console.WriteLine("10. Update a borrower");
+            Console.WriteLine("11. Delete a borrower");
+            Console.WriteLine("12. List all borrowers");
+            Console.WriteLine("13. Borrow a book");
+            Console.WriteLine("14. List all borrowed books");
             Console.WriteLine("16. Filter books by status");
             Console.WriteLine("\nEnter the number corresponding to the action you'd like to perform:");
         }
@@ -62,6 +115,12 @@ namespace LibraryManagement
         static void AddBook() {
             Console.WriteLine("Enter the book title:");
             string title = Console.ReadLine();
+            // Check if the book with the given title already exists
+            if (books.Any(book => book.Title == title))
+            {
+                Console.WriteLine("A book with the given title already exists in the library.");
+                return;
+            }
             Console.WriteLine("Enter the author's first name: ");
             string firstName = Console.ReadLine();
             Console.WriteLine("Enter the author's last name: ");
@@ -76,6 +135,7 @@ namespace LibraryManagement
 
             Author author = new Author { FirstName=firstName,LastName=lastName, DateOfBirth=dateOfBirth };  
             Book book = new Book { Title=title,Author=author,PublicationYear=publicationYear,IsAvailable=isAvailable};
+            
             books.Add(book);
 
         }
@@ -83,10 +143,62 @@ namespace LibraryManagement
 
             Console.WriteLine("Write title of the book that you want to update: ");
             string title = Console.ReadLine();
-            var book = books.FindAll(b => b.Title == title);
+            var book = books.Find(b => b.Title == title);
 
             if (book != null)
             {
+                Console.WriteLine("Enter the updated title (or press enter to keep the existing title):");
+                string updatedTitle = Console.ReadLine();
+                if (!string.IsNullOrEmpty(updatedTitle))
+                {
+                    book.Title = updatedTitle;
+                }
+
+                Console.WriteLine("Enter the updated author's first name (or press enter to keep the existing first name):");
+                string updatedFirstName = Console.ReadLine();
+                if (!string.IsNullOrEmpty(updatedFirstName))
+                {
+                    book.Author.FirstName = updatedFirstName;
+                }
+
+                Console.WriteLine("Enter the updated author's last name (or press enter to keep the existing last name):");
+                string updatedLastName = Console.ReadLine();
+                if (!string.IsNullOrEmpty(updatedLastName))
+                {
+                    book.Author.LastName = updatedLastName;
+                }
+
+                Console.WriteLine("Enter the updated publication year (or press enter to keep the existing publication year):");
+                string updatedPublicationYearString = Console.ReadLine();
+                if (!string.IsNullOrEmpty(updatedPublicationYearString))
+                {
+                    int updatedPublicationYear;
+                    if (int.TryParse(updatedPublicationYearString, out updatedPublicationYear))
+                    {
+                        book.PublicationYear = updatedPublicationYear;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Publication year not updated.");
+                    }
+                }
+
+                Console.WriteLine("Enter the updated availability status (true/false) (or press enter to keep the existing status):");
+                string updatedAvailabilityString = Console.ReadLine();
+                if (!string.IsNullOrEmpty(updatedAvailabilityString))
+                {
+                    bool updatedAvailability;
+                    if (bool.TryParse(updatedAvailabilityString, out updatedAvailability))
+                    {
+                        book.IsAvailable = updatedAvailability;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Availability status not updated.");
+                    }
+                }
+
+                Console.WriteLine("Book updated successfully!");
 
             }
 
@@ -102,12 +214,22 @@ namespace LibraryManagement
             if (book != null)
             {
                 books.Remove(book);
+                Console.WriteLine($"{title} remove from library sucessfully..!");
+            }
+            else
+            {
+                Console.WriteLine("Book with this title doesn't exists..!");
             }
         }
 
         static void ListAllBooks() {
+            if(books.Count() == 0)
+            {
+                Console.WriteLine("No books available in library..!");
+            }
             foreach (Book b in books) {
                 Console.WriteLine(b.ToString());
+
             }
         }
 
@@ -120,6 +242,225 @@ namespace LibraryManagement
             foreach (Book b in book)
             {
                 Console.WriteLine(b.ToString());
+            }
+        }
+
+        static void BorrowBook() {
+            Console.WriteLine("Enter the title of book that you want to borrow: ");
+            string title = Console.ReadLine() ;
+            var book = books.Find(b => b.Title == title);
+            if (book != null)
+            {
+                if (book.IsAvailable)
+                {
+                    Console.WriteLine("Enter your first name: ");
+                    string firstName = Console.ReadLine();
+                    Console.WriteLine("Enter your last name: ");
+                    string lastName = Console.ReadLine();
+                    Console.WriteLine("Enter your email: ");
+                    string email = Console.ReadLine();
+                    Console.WriteLine("Enter your phone number: ");
+                    string phoneNumber = Console.ReadLine();
+                    Borrower borrower = new Borrower { FirstName=firstName, LastName=lastName, Email=email,PhoneNumber=phoneNumber};
+                    book.IsAvailable = false;
+                    Console.WriteLine("Book borrowed succesfully..!");
+                    borrowers.Add(borrower);
+                }
+                else
+                {
+                    Console.WriteLine("The book you want is not available..!");
+                }
+            }
+
+        }
+        static void ListAllBorrowedBooks()
+        {
+            var borrowedBooks = books.Where(b => b.IsAvailable == false);
+            if (borrowedBooks != null)
+            {
+                foreach (var b in borrowedBooks)
+                {
+                    Console.WriteLine(b.ToString());
+                }
+            }
+            else
+            {
+                Console.WriteLine("No books is borrowed..!");
+            }
+        }
+
+
+        //Add author
+        static void AddAuthor() {
+            Console.WriteLine("Enter the author's first name: ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter the author's last name: ");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Ener the author's date of birth (YYY--MM-DD): ");
+            DateTime dateOfBirth = DateTime.Parse(Console.ReadLine());
+           
+            if (authors.Any(a => a.FirstName == firstName && a.LastName == lastName && a.DateOfBirth == dateOfBirth))
+            {
+                Console.WriteLine("Author already exists..!");
+                return;
+            }
+            Author author = new Author { FirstName = firstName, LastName = lastName, DateOfBirth = dateOfBirth };
+            authors.Add(author);
+            Console.WriteLine("author added succesfuly..!");
+        }
+        //Update author
+        static void UpdateAuthor() {
+
+            Console.WriteLine("Write a first name of author that you want to update: ");
+            string fName = Console.ReadLine();
+            Console.WriteLine("Write a last name of author that you want to udpate: ");
+            string lName = Console.ReadLine();
+
+            var author = authors.Find(a => a.FirstName == fName && a.LastName == lName);
+            if(author != null)
+            {
+                Console.WriteLine("Enter the updated author's first name (or press enter to keep the existing first name):");
+                string updatedFirstName = Console.ReadLine();
+                if (!string.IsNullOrEmpty(updatedFirstName))
+                {
+                    author.FirstName = updatedFirstName;
+                }
+
+                Console.WriteLine("Enter the updated author's last name (or press enter to keep the existing last name):");
+                string updatedLastName = Console.ReadLine();
+                if (!string.IsNullOrEmpty(updatedLastName))
+                {
+                    author.LastName = updatedLastName;
+                }
+                Console.WriteLine("Author updated successfully!");
+            }
+            else
+            {
+                Console.WriteLine("Author doesn't exists..!");
+            }
+            
+
+        }
+        //Remove author
+        static void DeleteAuthor()
+        {
+            Console.WriteLine("Write first name of author that you want to delete: ");
+            string fName = Console.ReadLine();
+            Console.WriteLine("Write last name of author that you want to delete: ");
+            string lName = Console.ReadLine();
+            
+
+            var author = authors.Find(a => a.FirstName==fName && a.LastName ==lName);
+
+            if (author != null)
+            {
+                authors.Remove(author);
+                Console.WriteLine($"{fName} {lName} remove from authors list..!");
+            }
+            else
+            {
+                Console.WriteLine("author with this name doesn't exists..!");
+            }
+
+        }
+        //List author
+        static void ListAllAuthor() {
+            if (authors.Count() == 0)
+            {
+                Console.WriteLine("No authors available in library..!");
+            }
+            foreach (var author in authors)
+            {
+                Console.WriteLine(author.ToString());
+            }
+        }
+        //add borrower
+        static void AddBorrower() {
+            Console.WriteLine("Enter your first name: ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter your last name: ");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter your email: ");
+            string email = Console.ReadLine();
+            Console.WriteLine("Enter your phone number: ");
+            string phoneNumber = Console.ReadLine();
+            Borrower borrower = new Borrower { FirstName = firstName, LastName = lastName, Email = email, PhoneNumber = phoneNumber };
+            borrowers.Add(borrower);
+            Console.WriteLine("borrower added succesfuly..!");
+        }
+        //update borrower
+        static void UpdateBorrower() {
+            Console.WriteLine("Write a first name of borrower that you want to update: ");
+            string fName = Console.ReadLine();
+            Console.WriteLine("Write a last name of borrower that you want to udpate: ");
+            string lName = Console.ReadLine();
+
+            var borrower = borrowers.Find(b => b.FirstName == fName && b.LastName == lName);
+
+            if (borrower != null)
+            {
+                Console.WriteLine("Enter the updated borrower's first name (or press enter to keep the existing first name):");
+                string updatedFirstName = Console.ReadLine();
+                if (!string.IsNullOrEmpty(updatedFirstName))
+                {
+                    borrower.FirstName = updatedFirstName;
+                }
+
+                Console.WriteLine("Enter the updated borrower's last name (or press enter to keep the existing last name):");
+                string updatedLastName = Console.ReadLine();
+                if (!string.IsNullOrEmpty(updatedLastName))
+                {
+                    borrower.LastName = updatedLastName;
+                }
+                Console.WriteLine("Enter the updated borrower's email (or press enter to keep the existing email):");
+                string updatedEmail = Console.ReadLine();
+                if (!string.IsNullOrEmpty(updatedEmail))
+                {
+                    borrower.Email = updatedEmail;
+                }
+                Console.WriteLine("Enter the updated borrower's phone number (or press enter to keep the existing phone number):");
+                string updatedPhoneNumber = Console.ReadLine();
+                if (!string.IsNullOrEmpty(updatedPhoneNumber))
+                {
+                    borrower.PhoneNumber = updatedEmail;
+                }
+                Console.WriteLine("Borrower updated successfully!");
+            }
+            else {
+                Console.WriteLine("Borrower doesn't exists..");
+            }
+
+
+        }
+        //delete borrower
+        static void DeleteBorrower() {
+
+            Console.WriteLine("Write first name of borrower that you want to delete: ");
+            string fName = Console.ReadLine();
+            Console.WriteLine("Write last name of borrower that you want to delete: ");
+            string lName = Console.ReadLine();
+
+
+            var borrower = borrowers.Find(b => b.FirstName == fName && b.LastName == lName);
+
+            if (borrower != null)
+            {
+                borrowers.Remove(borrower);
+                Console.WriteLine($"{fName} {lName} remove from borrowers list..!");
+            }
+            else
+            {
+                Console.WriteLine("borrower with this name doesn't exists..!");
+            }
+        }
+        //list borrower
+        static void ListAllBorrower() {
+            if(borrowers.Count==0) {
+                Console.WriteLine("No borrowers available in library..!");
+            }
+            foreach (var borrower in borrowers)
+            {
+                Console.WriteLine(borrower.ToString());
             }
         }
     }
@@ -143,6 +484,11 @@ namespace LibraryManagement
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime DateOfBirth { get; set; }
+        public override string ToString()
+        {
+            return $"Name: {FirstName} {LastName}\nDate of Birth: {DateOfBirth}";
+        }
+
     }
 
     class Borrower
@@ -151,5 +497,10 @@ namespace LibraryManagement
         public string LastName { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {FirstName} {LastName}\nEmail: {Email}\nPhone number: {PhoneNumber}";
+        }
     }
 }
